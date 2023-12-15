35228e3d0c29f2fd1a7d2fad4ce8bb3982fd7f6e
SheraCode <johannesssipayung27@gmail.com>
2023-12-15 21:07:52 +0700

    url mongodb

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection  = mongoose.createConnection('mongodb://localhost:27017/TodoApp').on('open',()=> {
+const connection  = mongoose.createConnection('mongodb://mongo:-5C6b2FCGDBcA14AAc5GB4acgE12fH6h@roundhouse.proxy.rlwy.net:36342').on('open',()=> {
     console.log("MongoDB Tersambung");
 }).on('error',()=> {
     console.log("MongoDB Tidak Tersambung");
